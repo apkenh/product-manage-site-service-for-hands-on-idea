b21c5092d3897e7f1150c278e1b14bb9a64d344f
kazuya <kenhks2004@gmail.com>
2022-08-16 21:49:11 +0800

    Add base project files

---
+++ README.md
@@ -0,0 +1,10 @@
+# product-manage-site-service-for-hands-on-idea
+
+Pls. refer orginal https://github.com/TetsujiFurukawa/product-manage-site-service-for-hands-on
+
+History
+
+20220816
+1. recreate the project by using intellij idea
+2. use latest spring boot 3 version
+3. use jdk version 17
---
+++ src/main/java/com/example/demo/productmanagesiteserviceforhandsonidea/ProductManageSiteServiceForHandsOnIdeaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.productmanagesiteserviceforhandsonidea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManageSiteServiceForHandsOnIdeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductManageSiteServiceForHandsOnIdeaApplication.class, args);
+    }
+
+}
